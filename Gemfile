83c73b2ad0544dea0e65089f5b2497e01fa1ec24
Dustin Robertson <dustinuriah@gmail.com>
2017-08-21 20:27:06 -0600

    display git config

---
+++ Gemfile
@@ -0,0 +1,3 @@
+
+# Added at 2017-08-21 20:08:29 -0600 by admin:
+gem "colorize", "~> 0.8.1"
--- main.rb
+++ main.rb
@@ -1,30 +1,60 @@
+# require 'colorize'
+# require_relative 'git'
+#
+# class Main
+#   include git
+#
+#   def puts_git(cmd)
+#     puts `git #{cmd} -h`
+#     menu
+#   end
+#
+#   def menu
+#   puts 'MAIN MENU'.colorize(:blue)
+#   puts '1 Enter Git Command'.colorize(:cyan)
+#   puts '2 Exit'.colorize(:cyan)
+#   choice = gets.to_i
+#   case choice
+#     when 1
+#       puts 'Enter a git command'.colorize(:green)
+#       Git.puts_git(gets.strip)
+#       menu
+#     when 2
+#       exit
+#     else
+#       puts 'Inavlid Choice'.colorize(:red)
+#       menu
+#     end
+#   end
+#
+# menu
+
 require 'colorize'
-require_relative 'git'
 
-class Main
-  include git
+def puts_git(cmd)
+  puts `git #{cmd} -h`
+  menu
+end
 
-  def puts_git(cmd)
-    puts `git #{cmd} -h`
-    menu
-  end
-
-  def menu
-  puts 'MAIN MENU'.colorize(:blue)
+def menu
+  puts 'MAIN MENU'.colorize(:cyan)
   puts '1 Enter Git Command'.colorize(:cyan)
-  puts '2 Exit'.colorize(:cyan)
+  puts '2 View Git Config'.colorize(:cyan)
+  puts '3 Exit'.colorize(:cyan)
   choice = gets.to_i
   case choice
     when 1
       puts 'Enter a git command'.colorize(:green)
-      Git.puts_git(gets.strip)
+      puts_git(gets.strip)
       menu
     when 2
+      Git.config
+    when 3
       exit
     else
       puts 'Inavlid Choice'.colorize(:red)
       menu
-    end
   end
+end
 
 menu
